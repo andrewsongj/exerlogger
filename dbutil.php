d6cd1e7c205ce2bcf98cedf05632ad298f627a2b
clarkmollencop <cmollencop@gmail.com>
2021-12-01 17:10:05 -0500

    db security info
    changed user that accesses the db to a more restricted user

--- dbutil.php
+++ dbutil.php
@@ -1,13 +1,16 @@
 <?php
 class DbUtil{
-	// public static $loginUser = "joyul"; 
-	// public static $loginPass = "whdbfl<33";
-	// public static $host = "localhost:3307"; // local host
-	// public static $schema = "final"; // DB Schema
-	public static $loginUser = "cm4qrk"; 
+	//superuser
+	/*public static $loginUser = "cm4qrk"; 
 	public static $loginPass = "DBPassword!";
-	public static $host = "mysql01.cs.virginia.edu"; // local host
-	public static $schema = "cm4qrk_final"; // DB Schema
+	public static $host = "mysql01.cs.virginia.edu";
+	public static $schema = "cm4qrk";*/ // DB Schema
+
+	//normal user
+	public static $loginUser = "cm4qrk_a"; 
+	public static $loginPass = "Fall2021!!";
+	public static $host = "mysql01.cs.virginia.edu";
+	public static $schema = "cm4qrk"; // DB Schema
   
 	public static function loginConnection(){
 		$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
