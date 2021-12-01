1a29e42f89f4a3191c4ec29ead5f08fe18413d33
clarkmollencop <cmollencop@gmail.com>
2021-12-01 15:03:34 -0500

    change db name

--- dbutil.php
+++ dbutil.php
@@ -7,7 +7,7 @@ class DbUtil{
 	public static $loginUser = "cm4qrk"; 
 	public static $loginPass = "DBPassword!";
 	public static $host = "mysql01.cs.virginia.edu"; // local host
-	public static $schema = "cm4qrk_final"; // DB Schema
+	public static $schema = "cm4qrk"; // DB Schema
   
 	public static function loginConnection(){
 		$db = new mysqli(DbUtil::$host, DbUtil::$loginUser, DbUtil::$loginPass, DbUtil::$schema);
